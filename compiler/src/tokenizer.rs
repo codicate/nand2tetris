ec1d0ff848bcea3d145bf9f7f5e0ac7194fb79fd
Henry Liu <codicate@gmail.com>
2025-06-12 07:22:39 -0400

    consume, peek, expect

--- compiler/src/tokenizer.rs
+++ compiler/src/tokenizer.rs
@@ -1,5 +1,5 @@
-#[derive(Debug)]
-enum TokenType {
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum TokenType {
     Keyword,
     Symbol,
     IntegerConstant,
@@ -7,7 +7,7 @@ enum TokenType {
     Identifier,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Token {
     type_: TokenType,
     content: String,
@@ -20,6 +20,7 @@ pub struct Tokenizer {
     path: String,
     content: Vec<char>,
     newline_indices: Vec<usize>,
+    peeked: Option<Token>,
 }
 
 impl Tokenizer {
@@ -90,6 +91,7 @@ impl Tokenizer {
             path,
             content: content.chars().collect(),
             newline_indices: Self::newline_indices(&content),
+            peeked: None,
         }
     }
 
@@ -133,64 +135,72 @@ impl Tokenizer {
         return self.idx < self.content.len();
     }
 
-    pub fn advance(&mut self) -> Option<Token> {
-        if !self.has_more_tokens() {
-            return None;
-        }
+    fn handle_whitespace(&mut self) -> Option<Token> {
+        self.idx += 1;
+        None
+    }
 
+    fn handle_string_constant(&mut self) -> Option<Token> {
+        let rest = &self.content[self.idx + 1..];
+        if let Some(pos) = rest.iter().position(|&c| c == '"') {
+            let string_constant = &rest[..pos];
+            self.idx += pos + 2; // skip over opening and closing quotes
+            Some(self.new_token(TokenType::StringConstant, string_constant.iter().collect()))
+        } else {
+            self.error("unclosed double quote \"");
+        }
+    }
+
+    fn handle_single_line_comment(&mut self) -> Option<Token> {
+        let rest = &self.content[self.idx + 2..];
+        if let Some(pos) = rest.iter().position(|&c| c == '\n') {
+            self.idx += pos + 2;
+        } else {
+            self.idx = self.content.len();
+        }
+        None
+    }
+
+    fn handle_multi_line_comment(&mut self) -> Option<Token> {
+        let rest = &self.content[self.idx + 2..];
+        if let Some(pos) = rest.windows(2).position(|w| w == ['*', '/']) {
+            self.idx += pos + 4;
+        } else {
+            self.error("unclosed multi line comment /*");
+        }
+        None
+    }
+
+    fn handle_slash(&mut self) -> Option<Token> {
+        let next = self.content[self.idx + 1];
+        match next {
+            '/' => self.handle_single_line_comment(),
+            '*' => self.handle_multi_line_comment(),
+            _ => self.handle_general_symbol('/'),
+        }
+    }
+
+    fn handle_general_symbol(&mut self, cur: char) -> Option<Token> {
+        self.idx += 1;
+        Some(self.new_token(TokenType::Symbol, Self::escape_xml_char(cur)))
+    }
+
+    // Consumes the next token and returns it
+    // Will return None if the next token is whitespace or a comment! (or no more tokens)
+    fn consume(&mut self) -> Option<Token> {
         let cur = self.content[self.idx];
+
+        // whitespace, comments, symbols, and string constants
         if Self::SYMBOLS.contains(&cur) {
-            // ignore whitespace and new lines
-            if [' ', '\n', '\r', '\t'].contains(&cur) {
-                self.idx += 1;
-                return None;
-            }
-
-            // string constant
-            if cur == '"' {
-                let rest = &self.content[self.idx + 1..];
-                if let Some(pos) = rest.iter().position(|&c| c == '"') {
-                    let string_constant = &rest[..pos];
-                    self.idx += pos + 2; // skip over ""
-                    return Some(
-                        self.new_token(TokenType::StringConstant, string_constant.iter().collect()),
-                    );
-                } else {
-                    self.error("unclosed double quote \"");
-                }
-            }
-
-            // check if we landed on a comment and skip over comment content
-            if cur == '/' && self.idx + 1 < self.content.len() {
-                let next = self.content[self.idx + 1];
-                let rest = &self.content[self.idx + 2..];
-
-                // single line comment //
-                if next == '/' {
-                    if let Some(pos) = rest.iter().position(|&c| c == '\n') {
-                        self.idx += pos + 2; // skip over //
-                    } else {
-                        self.idx = self.content.len();
-                    }
-                    return None;
-
-                // multi line comment /**/
-                } else if next == '*' {
-                    if let Some(pos) = rest.windows(2).position(|w| w == ['*', '/']) {
-                        self.idx += pos + 4; // skip over /**/
-                    } else {
-                        self.error("unclosed multi line comment /*");
-                    }
-                    return None;
-                }
-            }
-
-            // symbol
-            self.idx += 1;
-            return Some(self.new_token(TokenType::Symbol, Self::escape_xml_char(cur)));
+            return match cur {
+                ' ' | '\n' | '\r' | '\t' => self.handle_whitespace(),
+                '"' => self.handle_string_constant(),
+                '/' if self.idx + 1 < self.content.len() => self.handle_slash(),
+                _ => self.handle_general_symbol(cur),
+            };
         }
 
-        // we consider the current string slice before encountering another symbol
+        // we consider the current string slice before encountering another symbol or whitespace
         let mut token = String::new();
         while self.has_more_tokens() && !Self::SYMBOLS.contains(&self.content[self.idx]) {
             token.push(self.content[self.idx]);
@@ -215,18 +225,51 @@ impl Tokenizer {
         }
     }
 
+    // Wraps `consume` to skip over whitespace and comments to always return a valid token
+    pub fn peek(&mut self) -> Option<Token> {
+        if self.peeked.is_some() {
+            return self.peeked.clone();
+        }
+        while self.has_more_tokens() {
+            if let Some(token) = self.consume() {
+                self.peeked = Some(token.clone());
+                return Some(token);
+            }
+        }
+        return None;
+    }
+
+    pub fn expect(&mut self, type_: TokenType, content: Option<&str>) -> Option<Token> {
+        if let Some(token) = self.peek() {
+            if token.type_ == type_ && content.map_or(true, |c| c == token.content) {
+                self.peeked = None; // Clear peeked token after consuming
+                return Some(token);
+            } else {
+                self.error(&format!(
+                    "expected {:?}({}), found {:?}({})",
+                    type_,
+                    content.unwrap_or("ANY"),
+                    token.type_,
+                    token.content
+                ));
+            }
+        } else {
+            self.error(&format!("expected {:?}, but no more tokens", type_));
+        }
+    }
+
     pub fn output(&mut self) -> String {
         let mut output = String::new();
         output.push_str("<tokens>\n");
 
         while self.has_more_tokens() {
-            if let Some(token) = self.advance() {
+            if let Some(token) = self.consume() {
                 let type_ = Self::lowercase_first_letter(&format!("{:?}", token.type_));
                 output.push_str(&format!("<{}> {} </{}>\n", type_, token.content, type_));
             }
         }
 
         output.push_str("</tokens>\n");
-        return output;
+        output
     }
 }
